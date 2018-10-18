4b94db05dd85680e1a88142150bcac20f2f4b92f
andrebatist <arsp93@gmail.com>
2018-10-18 20:48:40 +0400

    4.3. Вычисление площади треугольника [#9461]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -0,0 +1,60 @@
+package ru.job4j.condition;
+
+/**
+ * Программа вычмсления площади треугольника.
+ *
+ * @author Plaksin Arseniy (arsp93@mail.ru)
+ */
+public class Triangle {
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+       this.a=a;
+       this.b=b;
+       this.c=c;
+    }
+
+    /**
+     * Метод для подсчета полупериметра.
+     * @param ac расстояние между точками a c.
+     * @param ab расстояние между точками a b.
+     * @param bc расстояние между точками b c.
+     * @return Полупериметр.
+     */
+    public double period(double ab, double ac, double bc) {
+        return (ab + ac + bc) / 2;
+    }
+
+    /**
+     * Метод вычисляет площадь треугольника.
+     *
+     * @return площадь треугольника.
+     */
+    public double area() {
+        double rsl = -1;
+        double ab = this.a.distanceTo(this.b);
+        double ac = this.a.distanceTo(this.c);
+        double bc = this.b.distanceTo(this.c);
+
+        double p = this.period(ab, ac, bc);
+        if (this.exist(ab,ac,bc)) {
+            rsl = Math.sqrt(p*(p-ab)*(p-ac)*(p-bc));
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод проверяет можно ли построить треугольник с такими длинами сторон.
+     *
+     * @param ab Длина от точки a b.
+     * @param ac Длина от точки a c.
+     * @param bc Длина от точки b c.
+     * @return
+     */
+    private boolean exist(double ab, double ac, double bc) {
+        return ((ab<(ac+bc))&&(ac<(ab+bc))&&(bc<(ab+ac)));
+    }
+
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.*;
+
+/**
+ * Test.
+ *
+ * @author Plaksin Arseniy (arsp93@mail.ru)
+ * @version 0.1
+ * @since 18.10.2018
+ */
+public class TriangleTest {
+
+    /**
+     * Test на вычмсление площади треугольника.
+     */
+    @Test
+    public void whenAreaSetThreePointsThenTriangleArea() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        Point c = new Point(2, 0);
+
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = 2D;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+
+    /**
+     * Test на вычмсление площади треугольника с неправильными сторонами.
+     */
+    @Test
+    public void whenAreaSetThreePointsWrongThenTriangleArea() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 0);
+        Point c = new Point(2, 0);
+
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = -1;
+        assertThat(result, closeTo(expected, 0.1));
+    }
+}
