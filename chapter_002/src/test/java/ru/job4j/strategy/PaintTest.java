4a46147c41c4dcbb2cab7e4f7c1abc12f3e0ed09
andrebatist <arsp93@gmail.com>
2018-11-11 11:15:47 +0400

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/strategy/PaintTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.strategy;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -17,14 +19,35 @@ import static org.junit.Assert.*;
  */
 public class PaintTest {
     /**
+     * Дефолтный вывод в консоль.
+     */
+    private final PrintStream stdout = System.out;
+    /**
+     * Буфер для результата.
+     */
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    /**
+     * Символ разделителя.
+     */
+    private final String ln = System.lineSeparator();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+    /**
      * Тест на вывод квадрата.
      */
     @Test
     public void whenDrawSquare() {
-        String ln = System.lineSeparator();
-        PrintStream stdOut = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
         assertThat(
                 new String(out.toByteArray()),
@@ -41,7 +64,6 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdOut);
     }
 
     /**
@@ -49,10 +71,6 @@ public class PaintTest {
      */
     @Test
     public void whenDrawTriangle() {
-        String ln = System.lineSeparator();
-        PrintStream stdOut = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
         assertThat(
                 new String(out.toByteArray()),
@@ -69,6 +87,5 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdOut);
     }
 }
