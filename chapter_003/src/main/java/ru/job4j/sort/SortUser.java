f22e029ad5761f09979469d33ccb37ac697772d3
andrebatist <arsp93@gmail.com>
2018-12-06 21:38:45 +0400

    UNFINISHED 1. Организовать сортировку User [#10034]

---
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -0,0 +1,10 @@
+package ru.job4j.sort;
+
+import java.util.List;
+import java.util.Set;
+
+public class SortUser {
+    public Set<User> sort (List<User> users) {
+        return null;
+    }
+}
---
+++ chapter_003/src/main/java/ru/job4j/sort/User.java
@@ -0,0 +1,44 @@
+package ru.job4j.sort;
+
+import java.util.Objects;
+
+public class User implements Comparable<User>{
+    private String name;
+
+    private int age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return getAge() == user.getAge() &&
+                Objects.equals(getName(), user.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        return 0;
+    }
+}
