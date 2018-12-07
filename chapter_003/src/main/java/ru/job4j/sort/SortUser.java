88853d7fe694ecec53aa402604182abafbd8f0a7
aplaksin <aplaksin@ecocompany.ru>
2018-12-07 13:45:16 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,5 +1,6 @@
 package ru.job4j.sort;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -18,5 +19,38 @@ public class SortUser {
      */
     public Set<User> sort(List<User> users) {
         return new TreeSet<>(users);
+    }
+
+    /**
+     * Возвращает отсортированный по длине имени по возрастанию список пользователей.
+     *
+     * @param users Список пользователей.
+     * @return Отсортированный список.
+     */
+    public List<User> sortNameLength(List<User> users) {
+        users.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return Integer.compare(o1.getName().length(), o2.getName().length());
+            }
+        });
+        return users;
+    }
+
+    /**
+     * Возвращает отсортированный по имени и возрасту по возрастанию список пользователей.
+     *
+     * @param users Список пользователей.
+     * @return Отсортированный список.
+     */
+    public List<User> sortByAllFields(List<User> users) {
+        users.sort(new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int res = o1.getName().compareTo(o2.getName());
+                return res != 0 ? res : o1.compareTo(o2);
+            }
+        });
+        return users;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertThat;
  */
 public class SortUserTest {
     /**
-     * Тест на список из трех пользователей.
+     * Тест на список из трех пользователей по возрасту.
      */
     @Test
     public void when3Users() {
@@ -26,6 +26,37 @@ public class SortUserTest {
         List<User> users = new ArrayList<>(Arrays.asList(user, user2, user3));
         Set result = new SortUser().sort(users);
         Set expect = new TreeSet(Arrays.asList(user3, user2, user));
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Тест на список из 5 пользователей сортировка по длине имени.
+     */
+    @Test
+    public void when5UsersSortByNameLength() {
+        User user = new User("Frank", 25);
+        User user2 = new User("Michael", 25);
+        User user3 = new User("Fedor", 15);
+        User user4 = new User("Ivan", 11);
+        User user5 = new User("Petr", 30);
+        List<User> users = new ArrayList<>(Arrays.asList(user, user2, user3, user4, user5));
+        List result = new SortUser().sortNameLength(users);
+        List expect = new ArrayList(Arrays.asList(user4, user5, user, user3, user2));
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Тест на список из 4 пользователей сортировка по имени и возрасту.
+     */
+    @Test
+    public void when4UsersSortByAllFields() {
+        User user = new User("Сергей", 25);
+        User user2 = new User("Иван", 30);
+        User user3 = new User("Сергей", 20);
+        User user4 = new User("Иван", 25);
+        List<User> users = new ArrayList<>(Arrays.asList(user, user2, user3, user4));
+        List result = new SortUser().sortByAllFields(users);
+        List expect = new ArrayList(Arrays.asList(user4, user2, user3, user));
         assertThat(result, is(expect));
     }
 }
