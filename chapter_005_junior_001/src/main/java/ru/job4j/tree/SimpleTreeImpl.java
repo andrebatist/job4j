40b15cc104706117c5a51fbe58bb999b80cb783a
aplaksin <aplaksin@ecocompany.ru>
2019-05-16 09:54:28 +0300

    2. Добавить метод boolean isBinary() [#1712]

--- chapter_005_junior_001/src/main/java/ru/job4j/tree/SimpleTreeImpl.java
+++ chapter_005_junior_001/src/main/java/ru/job4j/tree/SimpleTreeImpl.java
@@ -107,7 +107,7 @@ public class SimpleTreeImpl<T> implements SimpleTree<T> {
         NodeIterator it = new NodeIterator();
         while (it.hasNext()) {
             int numSubNodes = it.next().getChildren().size();
-            if (numSubNodes != 2 && numSubNodes != 0) {
+            if (numSubNodes > 2 || numSubNodes == 1) {
                 return false;
             }
         }
